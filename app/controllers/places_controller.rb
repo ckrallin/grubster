67ae0fdc9f04fce40d3fddc18e2a812761c340a6
Conner Krall <conner.krall@gmail.com>
2019-07-24 20:02:33 +0000

    create form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,9 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all
+    @places = Place.page(params[:page]).per_page(10)
+  end
+
+  def new
+    @place = Place.new
   end
 end
