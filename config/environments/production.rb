30c9d29b3e6c653db0678ce288edb7d5bc66b589
Conner Krall <conner.krall@gmail.com>
2019-07-25 19:55:13 +0000

    style sign in page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { 'grubster-conner-krall.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'grubster-conner-krall.herokuapp.com' }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
