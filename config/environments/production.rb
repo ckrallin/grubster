9d2f466c28a17a02e74e99e0fae4f7ea7924a0c6
Conner Krall <conner.krall@gmail.com>
2019-08-07 18:58:12 +0000

    mailing on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,17 @@
 Rails.application.configure do
+
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
+
+
+
   config.action_mailer.default_url_options = { host: 'grubster-conner-krall.herokuapp.com' }
 
   # Settings specified here will take precedence over those in config/application.rb.
